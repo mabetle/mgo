fe6546278c3dd7759248475f3550ed233190ddf3
Korben Zhang <korbenzhang@gmail.com>
2016-08-12 10:41:08 -0400

    add xml func

--- mcore/mxml/mask.go
+++ mcore/mxml/mask.go
@@ -2,6 +2,10 @@ package mxml
 
 import (
 	"encoding/xml"
+	"fmt"
+	"io"
+
+	"github.com/mabetle/mgo/mcore"
 )
 
 // Marshal returns string of obj xml format.
@@ -13,4 +17,24 @@ func Marshal(obj interface{}) string {
 
 	}
 	return string(b)
+}
+
+// Write to io.Writer
+func Write(obj interface{}, w io.Writer) error {
+	s := Marshal(obj)
+	if s == "" {
+		return fmt.Errorf("Parse object to xml error")
+	}
+	_, err := w.Write([]byte(s))
+	return err
+}
+
+// WriteFile write marshal xml to file
+func WriteFile(obj interface{}, file string) error {
+	s := Marshal(obj)
+	if s == "" {
+		return fmt.Errorf("Parse object to xml error")
+	}
+	_, err := mcore.WriteFile(file, s)
+	return err
 }
